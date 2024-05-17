c5da4d504dc134c0b71f3761cae05ed188610a21
NoemiCalzada <noemicalzadablanco@gmail.com>
2024-05-17 18:10:50 +0200

    tema5

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 // import GreetingF from './componentes/pure/GreetingF';
 import TaskListComponent from './componentes/container/task_list';  
 import Ejemplo1 from './hooks/Ejemplo1';
+import Ejemplo2 from './hooks/Ejemplo2';
+import MiComponenteConContexto from './hooks/Ejemplo3';
+import Ejemplo4 from './hooks/Ejemplo4';
 
 export default function App() {
   return (
@@ -12,7 +15,10 @@ export default function App() {
         {/* <Greeting name="Noemi"></Greeting> */}
         {/* <GreetingF name="Noemi Calzada"></GreetingF> */}
         {/* <TaskListComponent></TaskListComponent> */}
-        <Ejemplo1></Ejemplo1>
+        {<Ejemplo4 nombre="Noemí">
+          <h3>
+            Contenido del props.children</h3>
+            </Ejemplo4>}
       </header>
     </div>
   );
