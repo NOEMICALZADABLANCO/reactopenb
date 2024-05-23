b6e5d0b6da352f77521fc4517e5a34c05bbeb209
NoemiCalzada <noemicalzadablanco@gmail.com>
2024-05-23 22:22:25 +0200

    tema7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
+import './App.css';
 // import Greeting from './componentes/pure/greeting';
 // import GreetingF from './componentes/pure/GreetingF';
-import TaskListComponent from './componentes/container/task_list';  
+import TaskListComponent from './componentes/container/task_list';
 import Ejemplo1 from './hooks/Ejemplo1';
 import Ejemplo2 from './hooks/Ejemplo2';
 import MiComponenteConContexto from './hooks/Ejemplo3';
 import Ejemplo4 from './hooks/Ejemplo4';
+import GreetingStyled from './componentes/pure/forms/greetingStyled';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header"> */}
         <img src={logo} className="App-logo" alt="logo" />
         {/* <Greeting name="Noemi"></Greeting> */}
         {/* <GreetingF name="Noemi Calzada"></GreetingF> */}
-        {/* <TaskListComponent></TaskListComponent> */}
-        {<Ejemplo4 nombre="Noemí">
+        {/* <GreetingStyled name="Martín"></GreetingStyled> */}
+        {/* {<Ejemplo4 nombre="Noemí">
           <h3>
             Contenido del props.children</h3>
-            </Ejemplo4>}
-      </header>
+            </Ejemplo4>} */}
+            <TaskListComponent></TaskListComponent>
+      {/* </header> */}
     </div>
   );
 }
--- src/componentes/container/task_list.jsx
+++ src/componentes/container/task_list.jsx
@@ -1,14 +1,16 @@
 import React, {useEffect, useState} from 'react';
 import { LEVELS } from '../../models/levels.enum';
 import { Task } from '../../models/task.class';
-import TaskComponent from '../pure/task';
+import TaskComponent from '../pure/forms/task';
+import TaskForms from '../pure/forms/taskForms';
 
-export 
 const TaskListComponent = () => {
 
-  const defaultTask = new Task ('Example', 'Default description', false, LEVELS.NORMAL);
+  const defaultTask1 = new Task ('Example1', 'Description1', true, LEVELS.NORMAL);
+  const defaultTask2 = new Task ('Example2', 'Description2', false, LEVELS.URGENT);
+  const defaultTask3 = new Task ('Example3', 'Description3', false, LEVELS.BLOCKING);
 
-  const [task, setTask] = useState([defaultTask]);
+  const [task, setTask] = useState([defaultTask1, defaultTask2, defaultTask3]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -17,7 +19,7 @@ const TaskListComponent = () => {
     return () => {
       console.log('TaskList component is going to unmount...')
     }
-  }, [tasks])
+  }, [task])
 
 
     const changeState = (id) => {
@@ -26,14 +28,44 @@ const TaskListComponent = () => {
 
   return (
     <div>
-        <div>
-            Your task:
+        <div className='col-12'>
+          <div className="card" >
+            <div className='card-header p-3'>
+              <h5>
+              Your task:
+              </h5>
+            </div>
+
+            <div className='card-body' data-mdb-perfect-scrollbar='true' style={{position: 'relative', height: '400px'}}>
+              <table>
+                <thead>
+                  <tr>
+                    <th scope='col'>Title</th>
+                    <th scope='col'>Description</th>
+                    <th scope='col'>Priority</th>
+                    <th scope='col'>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {task.map((task, index) => {
+                    return(
+                      <TaskComponent 
+                      key={index}
+                      task={task}></TaskComponent>
+                    )
+                    }
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <TaskForms></TaskForms>
         </div>
-      <TaskComponent task={defaultTask}></TaskComponent>
     </div>
   )
 }
 
+export default TaskListComponent;
 
 
 
